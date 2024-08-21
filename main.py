5904ec8254b6db830b5c9f699128c42a5da5d981
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2024-08-21 12:15:51 +0530

    Player movement added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from settings import *
 from shader_program import ShaderProgram
 from scene import Scene
+from player import Player
 
 import moderngl as mgl
 import pygame as pg
@@ -33,16 +34,21 @@ class VoxelEngine():
         self.delta_time = 0
         self.time = 0
         
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+        
         self.is_running = True
         self.on_init()
         
         
     def on_init(self):
+        self.player = Player(self)
         self.shader_program = ShaderProgram(self)
         self.scene = Scene(self)
     
     
     def update(self):
+        self.player.update()
         self.shader_program.update()
         self.scene.update()
         self.delta_time = self.clock.tick()
