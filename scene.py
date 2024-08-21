7d5d15395662788816db0cf0eb954a0cda2fc1d3
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2024-08-21 14:54:18 +0530

    Chunk halfway done

--- scene.py
+++ scene.py
@@ -1,10 +1,10 @@
 from settings import *
-from meshes.quad_mesh import QuadMesh
+from world_objects.chunk import Chunk
 
 class Scene():
     def __init__(self, app):
         self.app = app
-        self.quad = QuadMesh(self.app)
+        self.chunk = Chunk(self.app)
     
     
     def update(self):
@@ -12,4 +12,4 @@ class Scene():
     
     
     def render(self):
-        self.quad.render()
+        self.chunk.render()
